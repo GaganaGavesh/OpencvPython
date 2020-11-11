3171d4f7bd6ef68e7005de9c26aabd95356e524d
Gagana Gavesh <gaveshgamage@gmail.com>
2020-11-11 14:39:31 +0530

    Made a script to apply Gaussian filter to captured images

--- venv/ApplyFilters.py
+++ venv/ApplyFilters.py
@@ -20,19 +20,4 @@ while img_counter < 51:
 
     cv2.waitKey(10)
 
-# img_name = "Central_Bus_Stand_2_730_830_{}.png".format(img_counter)
-# img = cv2.imread(img_name)
-#
-# g_frame = cv2.GaussianBlur(img, (3, 3), 0)
-# frame = cv2.medianBlur(g_frame, 5)
-#
-# cv2.imshow("test", frame)
-#
-# cv2.imwrite(img_name, frame)
-# print("{} written!".format(img_name))
-# img_counter += 1
-#
-# cv2.waitKey(0)
-# img.release()
-
 cv2.destroyAllWindows()
